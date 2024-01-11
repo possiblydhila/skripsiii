d3411e7e1fa99c0fbdd91e263fada4b34bb93151
ophinst <ophinst7004@gmail.com>
2024-01-11 13:53:07 +0700

    revert changes

--- download.py
+++ download.py
@@ -1,5 +1,5 @@
 import gdown
 
-url = "https://drive.google.com/file/d/1vobZ6KJZZQPh2RPJRgj3PwQpyLdPYKb-/view?usp=drive_link"
+url = "https://drive.google.com/file/d/1vobZ6KJZZQPh2RPJRgj3PwQpyLdPYKb-/view?usp=sharing"
 output = "best_model92.h5"
-gdown.download(url, output, quiet=False, fuzzy=True, use_cookies=True)
+gdown.download(url, output, quiet=False, fuzzy=True)
