aa34dcf1a0e07f8bcf8603d4a84fa4dd13c0df7e
ophinst <ophinst7004@gmail.com>
2024-01-11 13:09:28 +0700

    change gdown method

--- download.py
+++ download.py
@@ -1,5 +1,5 @@
 import gdown
 
-url = "https://drive.google.com/file/d/1vobZ6KJZZQPh2RPJRgj3PwQpyLdPYKb-/view"
+url = "1vobZ6KJZZQPh2RPJRgj3PwQpyLdPYKb-"
 output = "best_model92.h5"
-gdown.download(url, output, quiet=False, fuzzy=True)
+gdown.download(url, output, quiet=False, fuzzy=True, use_cookies=True)
