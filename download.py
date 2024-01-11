e1dff883d33953e4d153b5ccff2bb4c12da47b1b
ophinst <ophinst7004@gmail.com>
2024-01-11 09:21:28 +0700

    fix gdrive link

--- download.py
+++ download.py
@@ -1,5 +1,5 @@
 import gdown
 
-url = "https://drive.google.com/file/d/133fXBQknU0VCIdT-OmyyI24LpOfuNlHs/view?usp=sharing"
+url = "https://drive.google.com/file/d/133fXBQknU0VCIdT-OmyyI24LpOfuNlHs/view?usp=drive_link"
 output = "best_model92.h5"
 gdown.download(url, output, quiet=False, fuzzy=True)
