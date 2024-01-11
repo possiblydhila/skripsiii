a6349cd79cd51b82e59e0d0f52179edb994f0b40
ophinst <ophinst7004@gmail.com>
2024-01-11 09:35:10 +0700

    change model

--- download.py
+++ download.py
@@ -1,5 +1,5 @@
 import gdown
 
-url = "https://drive.google.com/file/d/133fXBQknU0VCIdT-OmyyI24LpOfuNlHs/view?usp=drive_link"
+url = "https://drive.google.com/file/d/1TygrsDOY9UxAoBEd5pzfKEC82oyPimqT"
 output = "best_model92.h5"
 gdown.download(url, output, quiet=False, fuzzy=True)
